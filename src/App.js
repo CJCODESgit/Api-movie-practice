5fc632193fbb1e5f897916f81b52f60aecf609b0
johncovenant <ufotjohnamos@gmail.com>
2023-06-22 13:37:10 +0100

    handling fall back content

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies. click fetch movies</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
