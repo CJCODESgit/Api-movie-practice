1020b855bd26eca157557528b7ed0556f5aa6588
johncovenant <ufotjohnamos@gmail.com>
2023-06-23 14:35:36 +0100

    created add movie form

--- src/App.js
+++ src/App.js
@@ -1,73 +1,71 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [isLoading, setisLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // adding useCallback to help us apply the function fetchmovieshandler as a dependency to useEffect
-
   const fetchMoviesHandler = useCallback(async () => {
-    setisLoading(true);
+    setIsLoading(true);
     setError(null);
     try {
-      //removed 's' after film in the fetch link correct link is https://swapi.dev/api/films just testing errors
-      const response = await fetch('https://swapi.dev/api/films');
-
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
-        throw new Error('hate to break it to you but something went wrong 😞...');
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
+          releaseDate: movieData.release_date,
+        };
       });
       setMovies(transformedMovies);
-      setisLoading(false);
     } catch (error) {
-      setError(error.message)
+      setError(error.message);
     }
-    setisLoading(false);
+    setIsLoading(false);
   }, []);
-
-  // useEffect to make us fetch data automatically without the button and we can also use button to reload manually.
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  let content = <p>uhm...i think you should click on the button above🤔</p>
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} />;
   }
 
   if (error) {
-    content = <p>{error}</p>
+    content = <p>{error}</p>;
   }
 
   if (isLoading) {
-    content = <p>oh right.. hang on...</p>
+    content = <p>Loading...</p>;
   }
-
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
